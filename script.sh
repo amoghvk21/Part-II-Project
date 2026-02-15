8b853b45df786c710c9ca070224b191e4312ee91
Amogh Vishwakarma <av670@cam.ac.uk>
2026-02-15 13:19:36 +0000

    removed mkdir from script

--- script.sh
+++ script.sh
@@ -5,8 +5,6 @@ set -eo pipefail
 # Always shut down the instance when the script exits (success or failure)
 trap 'sudo shutdown -h now' EXIT
 
-mkdir -p logs
-
 # echo "Running ddp finetuned llama job - lr=5e-6"
 # torchrun --nproc_per_node=4 --master_port=29500 "_2_llm_paper/ddp_finetune_llama_job.py" --learning_rate 5e-6 --save_dir "finetuned_llama_8b_lr5e6" 2>&1 | tee logs/lr5e6.log
 # echo "--------------------------------"
